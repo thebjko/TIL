cd191cea2f7dd6f29f8d590bcf75e67af70916fc
thebjko <thebjko@gmail.com>
2023-01-17 17:06:31 +0900

    update 1110.py - while문 사용. Memory optimized?

--- TIL-KDT/03_algorithm/17012023_practice/1110.py
+++ TIL-KDT/03_algorithm/17012023_practice/1110.py
@@ -1,15 +1,30 @@
 # 더하기 사이클
 # https://www.acmicpc.net/problem/1110
 
-ls = [int(input())]
+# ls = [int(input())]
 
-for i in ls:
-    a = i // 10   
-    b = i % 10
+# for i in ls:
+#     a = i // 10   
+#     b = i % 10
+#     c = a + b
+#     d = b * 10 + c % 10
+#     if d == ls[0]:
+#         print(len(ls))
+#         break
+
+#     ls.append(d)
+
+n = x = int(input())
+i = 1
+
+while True:
+    a = n // 10
+    b = n % 10
     c = a + b
-    d = b * 10 + c % 10
-    if d == ls[0]:
-        print(len(ls))
+    n = b * 10 + c % 10
+    if n == x:
+        print(i)
         break
-
-    ls.append(d)
+    else:
+        i += 1
+        
