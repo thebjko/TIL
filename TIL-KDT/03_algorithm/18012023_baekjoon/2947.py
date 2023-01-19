d5b54e41c4e048d317eff4d4c777004cdb2b3cd6
thebjko <thebjko@gmail.com>
2023-01-19 09:16:41 +0900

    나무조각 try except 구문 제거

--- TIL-KDT/03_algorithm/18012023_baekjoon/2947.py
+++ TIL-KDT/03_algorithm/18012023_baekjoon/2947.py
@@ -3,12 +3,6 @@ ls = list(open(0).read().split())
 
 while ls != ['1', '2', '3', '4', '5']:
     for i in range(len(ls) - 1):
-        try:
-            if ls[i] > ls[i + 1]:
-                ls[i], ls[i + 1] = ls[i + 1], ls[i]
-                print(*ls)
-            else:
-                continue
-        except:
-            continue
-        
+        if ls[i] > ls[i + 1]:
+            ls[i], ls[i + 1] = ls[i + 1], ls[i]
+            print(*ls)
